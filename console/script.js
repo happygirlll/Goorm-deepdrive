7997adf50fa4532196de945571dba62dae13e89a
happygirlll <lbm000314@gmail.com>
2024-11-26 00:00:24 +0900

    Initial commit

---
+++ console/script.js
@@ -0,0 +1,25 @@
+console.log('안녕하세요!');
+console.log(123);
+console.log(false);
+console.log(true);
+
+var greeting = '안녕하세요.';
+console.log(greeting);
+console.log({a:"a", b:"b"});
+
+console.table({a:"a", b:"b"});
+console.error('Error');
+console.warn('Warning!');
+
+console.time('hello');
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+//얼마나 시간 걸렸는지 보여줌
+console.timeEnd('hello');
+
+//모든 콘솔이 삭제됨
+//console.clear();
